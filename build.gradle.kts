6a336b28e0ad0b5e72907841c8556db938c182c1
Zsolt Kovacs <zs.myth@gmail.com>
2025-08-10 19:19:53 +0200

    fix vertx codegen issue
    - simplify parent gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,6 @@
 import org.gradle.api.JavaVersion.VERSION_21
 
 // Root project build file
-plugins {
-  id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-  id("io.freefair.lombok") version "8.14" apply false
-}
-
-val vertxVersion   = "5.0.2"
-val junitVersion   = "5.13.4"
-val jacksonVersion = "2.19.2"
-
 allprojects {
   group = "eu.describeit"
     version = "1.0.0-SNAPSHOT"
@@ -20,15 +11,6 @@ allprojects {
 }
 
 subprojects {
-  apply(plugin = "java")
-
-  dependencies {
-    add("implementation", platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
-    add("implementation", platform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
-
-    add("testImplementation", platform("org.junit:junit-bom:$junitVersion"))
-  }
-
   tasks.withType<JavaCompile> {
     sourceCompatibility = VERSION_21.toString()
     targetCompatibility = VERSION_21.toString()
--- kernel/build.gradle.kts
+++ kernel/build.gradle.kts
@@ -1,10 +1,15 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+//import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+val vertxVersion   = "5.0.2"
+val junitVersion   = "5.13.4"
+val jacksonVersion = "2.19.2"
 
 plugins {
   application
-  id("io.freefair.lombok")
-//  id("com.github.johnrengelman.shadow")
+  java
+  id("io.freefair.lombok") version "8.14"
+//  id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 val mainVerticleName = "eu.describeit.cristalise.kernel.MainVerticle"
@@ -15,33 +20,40 @@ application {
 }
 
 dependencies {
+  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+
   // Vert.x dependencies
-  implementation("io.vertx:vertx-launcher-application")
-  implementation("io.vertx:vertx-jdbc-client")
+//  implementation("io.vertx:vertx-launcher-application")
+//  implementation("io.vertx:vertx-jdbc-client")
 //  implementation("io.vertx:vertx-service-proxy")
   implementation("io.vertx:vertx-sql-client-templates")
   implementation("io.vertx:vertx-pg-client")
-  implementation("io.vertx:vertx-auth-properties")
-  implementation("io.vertx:vertx-hazelcast")
+//  implementation("io.vertx:vertx-auth-properties")
+//  implementation("io.vertx:vertx-hazelcast")
 
   // Vert.x codegen dependencies
-  compileOnly("io.vertx:vertx-codegen-json:5.0.2")
+  compileOnly("io.vertx:vertx-codegen-json")
   compileOnly("com.fasterxml.jackson.core:jackson-databind")
   compileOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-  annotationProcessor("io.vertx:vertx-codegen:5.0.2:processor")
+  annotationProcessor("io.vertx:vertx-codegen:$vertxVersion:processor")
+  annotationProcessor("io.vertx:vertx-sql-client-templates:$vertxVersion")
 
+//  implementation("org.slf4j:slf4j-api:2.0.12")
+//  implementation("qos.logback:logback-classic:1.5.3")
+
+  platform("org.junit:junit-bom:$junitVersion")
   testImplementation("io.vertx:vertx-junit5")
   testImplementation("org.junit.jupiter:junit-jupiter")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.withType<ShadowJar> {
-    archiveClassifier.set("fat")
-    manifest {
-        attributes(mapOf("Main-Verticle" to mainVerticleName))
-    }
-    mergeServiceFiles()
-}
+//tasks.withType<ShadowJar> {
+//    archiveClassifier.set("fat")
+//    manifest {
+//        attributes(mapOf("Main-Verticle" to mainVerticleName))
+//    }
+//    mergeServiceFiles()
+//}
 
 tasks.withType<Test> {
     useJUnitPlatform()
