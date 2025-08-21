62774bb3e1574757e52efd5258df212205f38568
Zsolt Kovacs <zs.myth@gmail.com>
2025-08-21 16:17:58 +0200

    Refactor common code of repository IT test classes

--- kernel/src/test/java/eu/describeit/cristalise/kernel/persistency/repository/ItemRepositoryIT.java
+++ kernel/src/test/java/eu/describeit/cristalise/kernel/persistency/repository/ItemRepositoryIT.java
@@ -1,35 +1,24 @@
 package eu.describeit.cristalise.kernel.persistency.repository;
 
 import eu.describeit.cristalise.kernel.persistency.domain.ItemDO;
-import io.vertx.core.Context;
-import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-import io.vertx.sqlclient.Pool;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 import static eu.describeit.cristalise.kernel.persistency.utils.DatabaseTestUtils.*;
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @Testcontainers(disabledWithoutDocker = true)
 @TestInstance(Lifecycle.PER_CLASS)
-class ItemRepositoryIT {
+class ItemRepositoryIT extends AbstractRepositoryIT {
 
-  private PostgreSQLContainer<?> pgContainer;
-
-  private Vertx vertx;
-  private Pool pool;
   private ItemRepository repository;
 
   final UUID uuidZero = UUID.fromString("00000000-0000-0000-0000-000000000000");
@@ -46,71 +35,10 @@ class ItemRepositoryIT {
   final long idParis = 2L;
 
   @BeforeAll
-  void setUpAll() throws Exception {
-    vertx = Vertx.vertx();
-    pgContainer = getPGContainer();
-    pgContainer.start();
-
-    pool = getPool(vertx, pgContainer);
-    liquibaseCreateTables(pgContainer);
-    liquibaseLoadTestData(pgContainer);
+  @Override
+  void setUpAll()throws Exception  {
+    super.setUpAll();
     repository = new ItemRepositoryImpl(pool);
-  }
-
-  @AfterAll
-  void tearDownAll() {
-    if (pool != null) {
-      pool.close();
-    }
-    if (vertx != null) {
-      vertx.close().toCompletionStage().toCompletableFuture().orTimeout(5, SECONDS).exceptionally(ex -> null);
-    }
-    pgContainer.stop();
-  }
-
-  private static <T> T awaitOrig(Future<T> future) {
-    try {
-      return future.toCompletionStage().toCompletableFuture().get(5, SECONDS);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private static <T> T await(Future<T> future) {
-    if (Context.isOnVertxThread()) {
-      // On the Vert.x event loop thread using virtual threads, block
-      log.info("await() - Vert.x event loop thread");
-      return Future.await(future);
-    }
-    else {
-      // On a platform thread, wait asynchronously
-      log.info("await() - Platform thread");
-      CompletableFuture<T> futureResult = new CompletableFuture<>();
-
-      future
-        .onComplete(ar -> {
-          if (ar.succeeded()) {
-            log.info("await(onComplete) - SUCCEEDED result:{}", ar.result());
-            futureResult.complete(ar.result());
-          } else if (ar.failed()) {
-            log.info("await(onComplete) - FAILED", ar.cause());
-            futureResult.completeExceptionally(ar.cause());
-          } else {
-            log.warn("await(onComplete) - ??????? result:{}", ar);
-            futureResult.completeExceptionally(new RuntimeException("Unexpected result: " + ar));
-          }
-        });
-
-      // blocks until the future is complete or the timeout is reached
-      try {
-        var result = futureResult.get(5, SECONDS);
-        log.info("await(get) - futureResult:{} result:{}", futureResult, result);
-        return result;
-      } catch (Exception e) {
-        log.error("await(get) - FAILED", e);
-        throw new RuntimeException(e);
-      }
-    }
   }
 
   @Test
--- kernel/src/test/java/eu/describeit/cristalise/kernel/persistency/utils/DatabaseTestUtils.java
+++ kernel/src/test/java/eu/describeit/cristalise/kernel/persistency/utils/DatabaseTestUtils.java
@@ -1,5 +1,7 @@
 package eu.describeit.cristalise.kernel.persistency.utils;
 
+import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.pgclient.PgBuilder;
 import io.vertx.pgclient.PgConnectOptions;
@@ -12,6 +14,10 @@ import liquibase.resource.ClassLoaderResourceAccessor;
 import lombok.extern.slf4j.Slf4j;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Slf4j
 public class DatabaseTestUtils {
@@ -75,5 +81,45 @@ public class DatabaseTestUtils {
       .setDatabase(pgContainer.getDatabaseName())
       .setUser(pgContainer.getUsername())
       .setPassword(pgContainer.getPassword());
+  }
+
+  public static <T> T awaitSimple(Future<T> future) {
+    try {
+      return future.toCompletionStage().toCompletableFuture().get(5, SECONDS);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static <T> T await(Future<T> future) {
+    if (Context.isOnVertxThread()) {
+      log.info("await() - Vert.x event loop thread");
+      return Future.await(future);
+    } else {
+      log.info("await() - Platform thread");
+      CompletableFuture<T> futureResult = new CompletableFuture<>();
+
+      future.onComplete(ar -> {
+        if (ar.succeeded()) {
+          log.info("await(onComplete) - SUCCEEDED result:{}", ar.result());
+          futureResult.complete(ar.result());
+        } else if (ar.failed()) {
+          log.info("await(onComplete) - FAILED", ar.cause());
+          futureResult.completeExceptionally(ar.cause());
+        } else {
+          log.warn("await(onComplete) - ??????? result:{}", ar);
+          futureResult.completeExceptionally(new RuntimeException("Unexpected result: " + ar));
+        }
+      });
+
+      try {
+        var result = futureResult.get(5, SECONDS);
+        log.info("await(get) - futureResult:{} result:{}", futureResult, result);
+        return result;
+      } catch (Exception e) {
+        log.error("await(get) - FAILED", e);
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
