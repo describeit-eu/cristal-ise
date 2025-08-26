36c653b2f5ddce597529550c11251b2d465353fd
Zsolt Kovacs <zs.myth@gmail.com>
2025-08-26 20:32:07 +0200

    fix field order for EventDO

--- kernel/src/main/java/eu/describeit/cristalise/kernel/persistency/domain/EventDO.java
+++ kernel/src/main/java/eu/describeit/cristalise/kernel/persistency/domain/EventDO.java
@@ -20,10 +20,9 @@ public final class EventDO {
 
   public EventDO() {}
 
-  @java.beans.ConstructorProperties({"id", "itemVersion", "actionDesc", "actionDescVersion", "script", "scriptVersion", "stateMachineDesc", "stateMachineVersion", "userLogin", "timestamp", "actionProperties", "itemId"})
-  public EventDO(Long id, String itemVersion, UUID actionDesc, String actionDescVersion, UUID script, String scriptVersion, UUID stateMachineDesc, String stateMachineVersion, String userLogin, LocalDateTime timestamp, String actionProperties, UUID itemId) {
+  @java.beans.ConstructorProperties({"id", "actionDesc", "actionDescVersion", "script", "scriptVersion", "stateMachineDesc", "stateMachineVersion", "userLogin", "timestamp", "actionProperties", "itemId", "itemVersion"})
+  public EventDO(Long id, UUID actionDesc, String actionDescVersion, UUID script, String scriptVersion, UUID stateMachineDesc, String stateMachineVersion, String userLogin, LocalDateTime timestamp, String actionProperties, UUID itemId, String itemVersion) {
     this.id = id;
-    this.itemVersion = itemVersion;
     this.actionDesc = actionDesc;
     this.actionDescVersion = actionDescVersion;
     this.script = script;
@@ -34,22 +33,22 @@ public final class EventDO {
     this.timestamp = timestamp;
     this.actionProperties = actionProperties;
     this.itemId = itemId;
+    this.itemVersion = itemVersion;
   }
 
-  @java.beans.ConstructorProperties({"itemVersion", "actionDesc", "actionDescVersion", "script", "scriptVersion", "stateMachineDesc", "stateMachineVersion", "userLogin", "timestamp", "actionProperties", "itemId"})
-  public EventDO(String itemVersion, UUID actionDesc, String actionDescVersion, UUID script, String scriptVersion, UUID stateMachineDesc, String stateMachineVersion, String userLogin, LocalDateTime timestamp, String actionProperties, UUID itemId) {
-    this(null, itemVersion, actionDesc, actionDescVersion, script, scriptVersion, stateMachineDesc, stateMachineVersion, userLogin, timestamp, actionProperties, itemId);
+  @java.beans.ConstructorProperties({"actionDesc", "actionDescVersion", "script", "scriptVersion", "stateMachineDesc", "stateMachineVersion", "userLogin", "timestamp", "actionProperties", "itemId", "itemVersion"})
+  public EventDO(UUID actionDesc, String actionDescVersion, UUID script, String scriptVersion, UUID stateMachineDesc, String stateMachineVersion, String userLogin, LocalDateTime timestamp, String actionProperties, UUID itemId, String itemVersion) {
+    this(null, actionDesc, actionDescVersion, script, scriptVersion, stateMachineDesc, stateMachineVersion, userLogin, timestamp, actionProperties, itemId, itemVersion);
   }
 
   public EventDO(EventDO other) {
-    this(null, other.itemVersion, other.actionDesc, other.actionDescVersion, other.script, other.scriptVersion, other.stateMachineDesc, other.stateMachineVersion, other.userLogin, other.timestamp, other.actionProperties, other.itemId);
+    this(null, other.actionDesc, other.actionDescVersion, other.script, other.scriptVersion, other.stateMachineDesc, other.stateMachineVersion, other.userLogin, other.timestamp, other.actionProperties, other.itemId, other.itemVersion);
   }
 
   @Override
   public boolean equals(Object o) {
     if (!(o instanceof EventDO other)) return false;
     return Objects.equals(this.getId(), other.getId()) &&
-           Objects.equals(this.getItemVersion(), other.getItemVersion()) &&
            Objects.equals(this.getActionDesc(), other.getActionDesc()) &&
            Objects.equals(this.getActionDescVersion(), other.getActionDescVersion()) &&
            Objects.equals(this.getScript(), other.getScript()) &&
@@ -59,22 +58,22 @@ public final class EventDO {
            Objects.equals(this.getUserLogin(), other.getUserLogin()) &&
            Objects.equals(this.getTimestamp(), other.getTimestamp()) &&
            Objects.equals(this.getActionProperties(), other.getActionProperties()) &&
-           Objects.equals(this.getItemId(), other.getItemId());
+           Objects.equals(this.getItemId(), other.getItemId()) &&
+           Objects.equals(this.getItemVersion(), other.getItemVersion());
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(getId(), getItemVersion(), getActionDesc(), getActionDescVersion(), getScript(), getScriptVersion(), getStateMachineDesc(), getStateMachineVersion(), getUserLogin(), getTimestamp(), getActionProperties(), getItemId());
+    return Objects.hash(getId(), getActionDesc(), getActionDescVersion(), getScript(), getScriptVersion(), getStateMachineDesc(), getStateMachineVersion(), getUserLogin(), getTimestamp(), getActionProperties(), getItemId(), getItemVersion());
   }
 
   @Override
   public String toString() {
-    return "EventDO[id=%s, itemVersion='%s', actionDesc=%s, actionDescVersion='%s', script=%s, scriptVersion='%s', stateMachineDesc=%s, stateMachineVersion='%s', userLogin='%s', timestamp=%s, actionProperties='%s', itemId=%s]"
-      .formatted(this.id, this.itemVersion, this.actionDesc, this.actionDescVersion, this.script, this.scriptVersion, this.stateMachineDesc, this.stateMachineVersion, this.userLogin, this.timestamp, this.actionProperties, this.itemId);
+    return "EventDO[id=%s, actionDesc=%s, actionDescVersion='%s', script=%s, scriptVersion='%s', stateMachineDesc=%s, stateMachineVersion='%s', userLogin='%s', timestamp=%s, actionProperties='%s', itemId=%s, itemVersion='%s']"
+      .formatted(this.id, this.actionDesc, this.actionDescVersion, this.script, this.scriptVersion, this.stateMachineDesc, this.stateMachineVersion, this.userLogin, this.timestamp, this.actionProperties, this.itemId, this.itemVersion);
   }
 
   public EventDO setId(Long id) { this.id = id; return this; }
-  public EventDO setItemVersion(String itemVersion) { this.itemVersion = itemVersion; return this; }
   public EventDO setActionDesc(UUID actionDesc) { this.actionDesc = actionDesc; return this; }
   public EventDO setActionDescVersion(String actionDescVersion) { this.actionDescVersion = actionDescVersion; return this; }
   public EventDO setScript(UUID script) { this.script = script; return this; }
@@ -85,9 +84,9 @@ public final class EventDO {
   public EventDO setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; return this; }
   public EventDO setActionProperties(String actionProperties) { this.actionProperties = actionProperties; return this; }
   public EventDO setItemId(UUID itemId) { this.itemId = itemId; return this; }
+  public EventDO setItemVersion(String itemVersion) { this.itemVersion = itemVersion; return this; }
 
   public Long getId() { return id; }
-  public String getItemVersion() { return itemVersion; }
   public UUID getActionDesc() { return actionDesc; }
   public String getActionDescVersion() { return actionDescVersion; }
   public UUID getScript() { return script; }
@@ -98,9 +97,9 @@ public final class EventDO {
   public LocalDateTime getTimestamp() { return timestamp; }
   public String getActionProperties() { return actionProperties; }
   public UUID getItemId() { return itemId; }
+  public String getItemVersion() { return itemVersion; }
 
   private Long id;
-  private String itemVersion;
   private UUID actionDesc;
   private String actionDescVersion;
   private UUID script;
@@ -111,4 +110,5 @@ public final class EventDO {
   private LocalDateTime timestamp;
   private String actionProperties;
   private UUID itemId;
+  private String itemVersion;
 }
